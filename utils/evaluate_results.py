123da8dcbbc9f3438ee1ce40d4b4387f34280188
Jin Huang <kiyoshi@Jins-MacBook-Pro-2019.local>
2021-08-06 00:08:06 -0400

    add auc

--- utils/evaluate_results.py
+++ utils/evaluate_results.py
@@ -4,22 +4,33 @@ import numpy as np
 
 
 # UCF101, softmax
-known_as_known = np.array([1026, 1026, 1023, 1022, 1020, 1016, 1003])
-known_as_unknown = np.array([0, 0, 3, 4, 6, 10, 23])
+# known_as_known = np.array([1026, 1026, 1023, 1022, 1020, 1016, 1003])
+# known_as_unknown = np.array([0, 0, 3, 4, 6, 10, 23])
+#
+# unknown_as_known = np.array([3142, 2098, 1719, 1461, 1227, 995, 634])
+# unknown_as_unknown = np.array([140, 1184, 1563, 1821, 2055, 2287, 2648])
 
-unknown_as_known = np.array([3142, 2098, 1719, 1461, 1227, 995, 634])
-unknown_as_unknown = np.array([140, 1184, 1563, 1821, 2055, 2287, 2648])
 
-fpr = (unknown_as_known)/(known_as_known+unknown_as_known)
-tpr = (known_as_known)/(known_as_known+unknown_as_known)
+# HMDB
+known_as_known = np.array([432, 404, 389, 373, 352])
+known_as_unknown = np.array([32, 60, 75, 91, 112])
 
-np.insert(fpr, 0, 0.0)
-np.insert(fpr, fpr.shape[0], 1.0)
+unknown_as_known = np.array([1103, 815, 613, 448, 275])
+unknown_as_unknown = np.array([757, 1045, 1247, 1412, 1585])
 
-np.insert(tpr, 0, 0.0)
-np.insert(tpr, fpr.shape[0], 1.0)
+
+fpr = (unknown_as_known)/(unknown_as_known+unknown_as_unknown)
+tpr = (known_as_known)/(known_as_known+known_as_unknown)
+
+# fpr = np.insert(fpr, 0, 1.0)
+# fpr = np.insert(fpr, fpr.shape[0], 0.0)
+#
+# tpr = np.insert(tpr, 0, 0.0)
+# tpr = np.insert(tpr, tpr.shape[0], 1.0)
 
 print(fpr)
 print(tpr)
+
+print("test")
 
 print(metrics.auc(fpr, tpr))
